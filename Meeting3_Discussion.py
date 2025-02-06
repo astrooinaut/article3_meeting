5ba5a42ae14b7a0286c003c3623b386bbc59cab4
astrooinaut <82753878+astrooinaut@users.noreply.github.com>
2025-02-06 18:22:32 +0200

    Added googlesheets

--- Meeting3_Discussion.py
+++ Meeting3_Discussion.py
@@ -7,7 +7,15 @@ Created on Wed Jan 29 11:58:13 2025
 """
 
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
 
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+df = conn.read()
+
+for row in df.itertuples():
+    st.write(f"{row.name} commented :{row.comment}:")
+    
 # Title of the app
 st.title("Article 3 Planning")
 
